7dc341e7092ce7e0ceda69350441220449ecb448
zekusu0426 <vagrant@localhost.localdomain>
2018-08-14 14:11:05 +0000

    Add button modal select_all

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,16 +29,29 @@ $(function() {
     }).trigger('change');
 
     // 全選択
-    $('.card_all_checkbox input,.card_all_checkbox label').click(function(){ //全選択・全解除をクリックしたとき
-        var items = $(this).closest('.card_all_checkbox').next().find('input');
-        // var items = $(this).closest('.card_checkbox_list');
-        // console.log($(this));
-        if($(this).is(':checked')) { //全選択・全解除がcheckedだったら
-            $(items).prop('checked', true); //アイテムを全部checkedにする
-        } else { //全選択・全解除がcheckedじゃなかったら
-            $(items).prop('checked', false); //アイテムを全部checkedはずす
-        }
-    });    
+    // $('.card_all_checkbox input,.card_all_checkbox label').click(function(){ //全選択・全解除をクリックしたとき
+    //     var items = $(this).closest('.card_all_checkbox').next().find('input');
+    //     // var items = $(this).closest('.card_checkbox_list');
+    //     // console.log($(this));
+    //     if($(this).is(':checked')) { //全選択・全解除がcheckedだったら
+    //         $(items).prop('checked', true); //アイテムを全部checkedにする
+    //     } else { //全選択・全解除がcheckedじゃなかったら
+    //         $(items).prop('checked', false); //アイテムを全部checkedはずす
+    //     }
+    // });
+    $(".btn-select_all").on('click', function(){ //全選択・全解除をクリックしたとき
+        $(this).closest('.modal-body').find('input').prop('checked', true);
+    });
+
+    $(".btn-all_cancellation").on('click', function(){ //全選択・全解除をクリックしたとき
+        $(this).closest('.modal-body').find('input').prop('checked', false);
+    });
+
+    $(".btn-select_all,.btn-all_cancellation").on('click', function() {
+        var cnt = $('#card_list .card_checkbox input:checkbox:checked').length;
+        $('div.possession_rate').text('所持率：' + Math.round(cnt*100*100/total)/100 + '％' );
+        $('div.selected_card').text('選択：' + cnt + '個');
+    }).trigger('change');   
 });
 
 
